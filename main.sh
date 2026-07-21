f78f701eb71cba4e9dab183f136cb1dd2be08630
theacebutler <theacebutler@gmail.com>
2026-07-21 19:26:56 -0400

    ADD get the resource usage for each process in the session

--- main.sh
+++ main.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
+
 # get all the windows in a session
 ttys=$(tmux list-windows -F "#{pane_tty}")
+declare -A stats
+
+# get the stats for each window/tty
+echo "$ttys"
+for tty in $ttys; do
+  # awk '{stats[cpu]+=$1; stats[mem]+=$2}'
+  data=$(ps -t "$tty" -o %cpu,%mem | grep -v "%CPU")
+  # echo "$data"
+  # stats[cpu]=$(cut -d' ' -f2 <<<"$data")
+  stats[cpu]=$(cut -d' ' -f2 <<<"$data")
+  stats[mem]=$(cut -d' ' -f4 <<<"$data")
+done
+echo "Total cpu usage: ${stats[cpu]}"
+echo "Total mem usage:  ${stats[mem]}"
